70fd0c2831125ae59b2547668c3796a18db20dcf
PBFD_TAN <jovan_pbcfd@panalobet.com>
2025-01-02 18:05:27 +0800

    Performance test

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import ResImage from './res-image'
+import Image from 'next/image'
 import type { StaticImageData } from 'next/image'
 
 export default function Hero({
@@ -15,20 +15,14 @@ export default function Hero({
     return (
         <section className="md:pb-16">
             <div className="max-w-7xl mx-auto py-[1rem] px-[10px] md:flex md:gap-5 md:flex-row-reverse justify-between items-center">
-                <div className="w-full flex items-center justify-center md:w-[40%]">
-                    <ResImage
-                        path={image}
-                        priority={true}
-                        mobileSize={{
-                            width: 200,
-                            height: 200,
-                        }}
-                        desktopSize={{
-                            width: 530,
-                            height: 530,
-                        }}
-                        alt="Home banner"
-                        screenSize={900}
+                <div className="relative w-full h-[200px] flex items-center justify-center  md:h-[500px] md:w-[40%]">
+                    <Image
+                        src={image}
+                        alt="Panalobet Main Hero Image"
+                        fill={true}
+                        style={{ objectFit: 'contain' }}
+                        sizes="(max-width: 768px) 100vw, 1920px"
+                        priority
                     />
                 </div>
                 <div className="w-full text-white md:w-[60%]">
@@ -41,12 +35,12 @@ export default function Hero({
                     <div className="mt-4 max-w-fit md:mt-8">
                         <button
                             className={`${buttonText ? '' : 'hidden'} text-[1.1rem] p-[0.5rem] w-full font-bold rounded-full
-                                    bg-gradient-to-br from-red-500 via-gray-700 to-blue-600 text-white shadow-lg
-                                    border border-gray-200 border-solid
-                                    transition-all duration-200 ease-out
-                                    hover:bg-gradient-to-br hover:from-blue-600 hover:via-gray-700 hover:to-red-500 hover:shadow-xl
-                                    hover:text-gray-100 hover:border-gray-300
-                                    focus:ring-4 focus:ring-red-500 focus:ring-offset-2 md:w-full md:p-[1rem]`}
+                                                bg-gradient-to-br from-red-500 via-gray-700 to-blue-600 text-white shadow-lg
+                                                border border-gray-200 border-solid
+                                                transition-all duration-200 ease-out
+                                                hover:bg-gradient-to-br hover:from-blue-600 hover:via-gray-700 hover:to-red-500 hover:shadow-xl
+                                                hover:text-gray-100 hover:border-gray-300
+                                                focus:ring-4 focus:ring-red-500 focus:ring-offset-2 md:w-full md:p-[1rem]`}
                         >
                             {buttonText}
                         </button>
