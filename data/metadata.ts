4721f5626f571dd48855d75f0cbd1f474f4cadb6
jovpbcfd <jovan_pbcfd@panalobet.com>
2025-01-29 14:06:40 +0800

    Update metadata

---
+++ data/metadata.ts
@@ -0,0 +1,34 @@
+export const homemetadata = {
+    title: 'Panalobet- Best Online Casino and Sports betting in Philippines',
+    description: 'Panalobet Casino a licensed online casino in Philippines, e-bingo, and sports betting platform in Philippines. Enjoy fast and secure deposits and withdrawals.',
+}
+
+export const regmetadata = {
+    title: 'Sign up and play at Panalobet and get 100% Welcome Bonus up',
+    description: 'Download the Panalobet App, Sign up now and get 100% Welcome Bonus, Enjoy your favourite games.'
+}
+
+export const withdrawalmetadata = {
+    title: 'Panalobet Safe and Secure Payment Deposit & Withdrawal',
+    description: 'Visit Panalobet and Learn about the payment process. Panalobet offers safe, secure and easy payment method for depost & Withdrawal.'
+}
+
+export const casinometadata = {
+    title: 'Panalobet Online Casino Philippines- Join Now and Earn Big Amount',
+    description: 'Join Panalobet online casino in the Philippines! Enjoy exciting slots, live dealers, and huge bonuses. Play safely and win real money now!'
+}
+
+export const supportmetadata = {
+    title: 'Panalobet Customer Support 24/7 Available',
+    description: 'Panalobet Customer Support available 24/7. You can contact the support team for any problem and ask for help in solving it as soon as possible. '
+}
+
+export const aboutmetadata = {
+    title: 'About Panalobet Online Casino',
+    description: 'Panalobet is a premier online casino in the Philippines, offering wide range of games included live casino games, slots, fishing games, sports betting and Bonuses.'
+}
+
+export const contactmetadata = {
+    title: 'Contact Panalobet Online Casino in Philippines',
+    description: 'If there are any problems, questions and commercial offers, users can contact the site administration directly by visit our website.'
+}
